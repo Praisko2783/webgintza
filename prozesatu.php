e2b9fcb4cf810e58ef200790541a6d41864e2e79
Praisko2783 <56635489+Praisko2783@users.noreply.github.com>
2024-06-28 11:33:03 +0200

    Add files via upload

---
+++ prozesatu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Webgunegintza</title>
+  <meta charset="UTF-8">
+  <title>Prozesatu</title>
+</head>
+
+<body>
+<section>
+    <p>
+        <?php
+            print('Balioa da: '. $_GET ['izena'] . '<br> eta adina da: '. $_GET ['adina']);
+        ?>
+    </p>
+</section>
+
+</body>
+</html>
